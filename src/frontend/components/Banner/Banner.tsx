2e689117fd1f76c9fa795a807437823dc868b52e
mikeyxoel <11162075+garciaba@users.noreply.github.com>
2026-02-25 17:50:06 +0100

    banner fixed

--- src/frontend/components/Banner/Banner.tsx
+++ src/frontend/components/Banner/Banner.tsx
@@ -11,8 +11,8 @@ const Banner = () => {
         <S.BannerImg />
       </S.ImageContainer>
       <S.TextContainer>
-        <S.Title>The best telescopes to see the world closer</S.Title>
-        <Link href="#hot-products"><S.GoShoppingButton>Go Shopping</S.GoShoppingButton></Link>
+        <S.Title>Contemporary essentials for every season.</S.Title>
+        <Link href="#hot-products"><S.GoShoppingButton>Shop Collection</S.GoShoppingButton></Link>
       </S.TextContainer>
     </S.Banner>
   );
